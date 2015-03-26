c26731c9a577d300669a308e5e5274b4cfe4e102
Dsntnow <dsntnow@hotmail.com>
2015-03-26 13:32:26 -0300

    Add restful api to user model

---
+++ routes/users.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var router = express.Router();
+var UserModel = require('../model/user').UserModel;
+
+
+router.post('/', function(req, res) {
+	var user = new UserModel({
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		email: req.body.email,
+		password: req.body.password
+	});
+
+	user.save(function(err) {
+		if (err) {
+			console.log(err);
+			res.status(400).end();
+		}
+
+		res.status(200).end();
+	});
+
+});
+
+
+router.get('/list', function(req, res) {
+	var l = parseInt(req.query.limit) || 1;
+	if (l <= 0) {
+		res.status(400).end();
+	} else {
+		UserModel.find({}).limit(l).exec(function (err, users) {
+    		if (err) {
+    			console.log(err);
+    			res.status(500).end();
+    		} else {
+    			res.json(users)
+    		}
+  		});
+	}
+});
+
+
+router.get('/total', function(req, res) {
+	
+	UserModel.count({}, function(err, count){
+		res.json({count: count});
+	});
+
+});
+
+
+router.get('/:id/show', function(req, res) {
+	var id = req.params.id;
+
+	if (id.lenth > 1024) {
+		res.status(400).end();
+	}
+
+	UserModel.findOne({ _id: id }, function(err, user) {
+		if (err) {
+			res.status(404).end();
+		}
+
+		res.json(user);
+	});
+
+});
+
+module.exports = router;
